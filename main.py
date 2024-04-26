f797882dfc0281cd65dfe759d6a24cffacf88324
Youssra1999 <100140863+Youssra1999@users.noreply.github.com>
2024-04-26 10:27:39 +0100

    Implemented K-means with differen tvalues of K

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import numpy as np
 import kmeans
 import common
-import naive_em
-import em
+import matplotlib.pyplot as plt
 
+# Load Toy Dataset
 X = np.loadtxt("toy_data.txt")
 
-# TODO: Your code here
+def run_kmeans():
+    for K in range(1, 5):
+        min_cost = None
+        best_seed = None
+        for seed in range(0, 5):
+            mixture, post = common.init(X, K, seed)
+            mixture, post, cost = kmeans.run(X, mixture, post)
+            if min_cost is None or cost < min_cost:
+                min_cost = cost
+                best_seed = seed
+
+        mixture, post = common.init(X, K, best_seed)
+        mixture, post, cost = kmeans.run(X, mixture, post)
+        title = f"K-means for K={K}, seed={best_seed}, cost={min_cost}"  # Use f-string for formatting
+        print(title)
+        common.plot(X, mixture, post, title)
+
+run_kmeans()
